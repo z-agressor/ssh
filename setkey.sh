0f3a6340ab7ab557199bb783935663baf4101483
z-agressor <119460527+z-agressor@users.noreply.github.com>
2022-12-08 02:44:25 +0300

    Create setkey.sh

---
+++ setkey.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Проверить наличие директории /root/.ssh/ и если нет, то создать
+if [ ! -d "/root/.ssh/" ]; then
+mkdir -p /root/.ssh/
+fi
+#Проверить наличие файла /root/.ssh/authorized_keys и если нет, то создать
+if [ ! -f "/root/.ssh/authorized_keys" ]; then
+touch /root/.ssh/authorized_keys
+fi
+#установить права на файл /root/.ssh/authorized_keys
+chmod 644 /root/.ssh/authorized_keys
+#установить права на директорию /root/.ssh/
+chmod 700 /root/.ssh/
+keys=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/authorized_keys | grep -oP 'href="\K[^"]+')
+echo $keys >> /root/.ssh/authorized_keys
+systemctl restart sshd
+exit
