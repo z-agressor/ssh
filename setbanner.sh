7f63d9f0874983b88d2770a0cf21d2bdacd65c67
z-agressor <119460527+z-agressor@users.noreply.github.com>
2022-12-08 04:20:39 +0300

    Update setbanner.sh

--- setbanner.sh
+++ setbanner.sh
@@ -4,4 +4,5 @@ banner=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/baners.sh
 echo $banner > /etc/profile.d/baners.sh
 chmod +x /etc/profile.d/baners.sh
 systemctl restart sshd
+./configubnt.sh
 exit
