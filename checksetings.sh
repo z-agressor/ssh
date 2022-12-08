cfb5971108a1e6c11923e11884a7c2280fcf5594
z-agressor <119460527+z-agressor@users.noreply.github.com>
2022-12-08 04:20:22 +0300

    Update checksetings.sh

--- checksetings.sh
+++ checksetings.sh
@@ -1,36 +1,58 @@
 #!/bin/bash
+#Colours
+red="\033[00;31m"
+RED="\033[01;31m"
+green="\033[00;32m"
+GREEN="\033[01;32m"
+yellow="\033[00;33m"
+YELLOW="\033[01;33m"
+blue="\033[00;34m"
+BLUE="\033[01;34m"
+magenta="\033[00;35m"
+MAGENTA="\033[01;35m"
+cyan="\033[00;36m"
+CYAN="\033[01;36m"
+white="\033[00;37m"
+WHITE="\033[01;37m"
+#Конец цветов
+#Установить значения переменных
+keys=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/authorized_keys | grep -oP 'href="\K[^"]+')
+config=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/sshd_config | grep -oP 'href="\K[^"]+')
+banner=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/baners.sh | grep -oP 'href="\K[^"]+')
 #Проверка
 #Назначить переменной значение из файла /root/.ssh/authorized_keys
 testkeys=$(cat /root/.ssh/authorized_keys)
 #Сравнить ключи с значениями переменной keys
 if [ "$testkeys" = "$keys" ]; then
-echo "Ключи SSH установлены"
+echo -e "${GREEN} Ключи SSH установлены"
 else
-echo "Ключи SSH не установлены"
+echo -e "${RED}Ключи SSH не установлены"
 fi
 #Назначить переменной значение из файла /etc/ssh/sshd_config
 testconfig=$(cat /etc/ssh/sshd_config)
 #Сравнить ключи с значениями переменной keys
 if [ "$testconfig" = "$config" ]; then
-echo "Конфигурация SSH установлена"
+echo -e "${GREEN}Конфигурация SSH установлена"
 else
-echo "Конфигурация SSH не установлена"
+echo -e "${RED}Конфигурация SSH не установлена"
 fi
 #Назначить переменной значение из файла /etc/profile.d/baners.sh
 testbanner=$(cat /etc/profile.d/baners.sh)
 #Сравнить ключи с значениями переменной keys
 if [ "$testbanner" = "$banner" ]; then
-echo "Банеры установлены"
+echo -e "${GREEN}Банеры установлены"
 else
-echo "Банеры не установлены"
+echo -e "${RED}Банеры не установлены"
 fi
 #Выыести список правил ufw
 ufw status
 #Выыести список правил iptables
 iptables -L -n -v
-#Вывести значение forward в файле /proc/sys/net/ipv4/ip_forward
-cat /proc/sys/net/ipv4/ip_forward
-#Вывести значение forward в файле /etc/sysctl.conf
-cat /etc/sysctl.conf
+#Получить строку из файла /etc/sysctl.conf с параметром net.ipv4.ip_forward
+testipf=$(cat /etc/sysctl.conf | grep net.ipv4.ip_forward)
+echo -e "{$MAGENTA} $testipf"
+#Выыести на экран значение из файла /proc/sys/net/ipv4/ip_forward
+echo -e "{$MAGENTA} cat /proc/sys/net/ipv4/ip_forward"
+./configubnt.sh
 #Выход
 exit
