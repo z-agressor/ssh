ec8ffe71f41ad7b96443eb959097dfed4ae5860c
z-agressor <119460527+z-agressor@users.noreply.github.com>
2023-01-25 21:59:54 +0300

    Update setbanner.sh

--- setbanner.sh
+++ setbanner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Настроить баннер SSH сервера
-banner=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/baners.sh | grep -oP 'href="\K[^"]+')
+banner=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/baners.sh)
 echo $banner > /etc/profile.d/baners.sh
 chmod +x /etc/profile.d/baners.sh
 systemctl restart sshd
