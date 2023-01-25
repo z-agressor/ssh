3364d0fef52c9cdd387320631dc8c754a1ac5d78
z-agressor <119460527+z-agressor@users.noreply.github.com>
2023-01-25 22:19:18 +0300

    Update setsshconfig.sh

--- setsshconfig.sh
+++ setsshconfig.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #перезаписать файл /etc/ssh/sshd_config
+#Очистка содержимого файла перед записью в него
+cp /dev/null > /etc/ssh/sshd_config
 config=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/sshd_config)
-echo $config > /etc/ssh/sshd_config
+echo "$config" > /etc/ssh/sshd_config
 systemctl restart sshd
 ./configubnt.sh
 exit
