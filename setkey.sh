f7e9071a3bb9511a48bf5837172e273b46c19027
z-agressor <119460527+z-agressor@users.noreply.github.com>
2023-01-25 22:16:55 +0300

    Update setkey.sh

--- setkey.sh
+++ setkey.sh
@@ -11,7 +11,10 @@ fi
 chmod 644 /root/.ssh/authorized_keys
 #установить права на директорию /root/.ssh/
 chmod 700 /root/.ssh/
+#Очистка содержимого файла перед записью в него
+cp /dev/null > /root/.ssh/authorized_keys
 keys=$(curl -s https://raw.githubusercontent.com/z-agressor/ssh/main/authorized_keys)
+#Для того что бы echo учитывала переносы строк итд должен быть в ковычках
 echo "$keys" >> /root/.ssh/authorized_keys
 systemctl restart sshd
 ./configubnt.sh
